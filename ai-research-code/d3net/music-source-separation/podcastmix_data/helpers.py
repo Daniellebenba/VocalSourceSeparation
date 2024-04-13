5b46668d2926dd5bf3740841892fae5465b7f4e3
Danielle Ben Bashat <daniellebenba@gmail.com>
2024-04-13 14:01:10 +0300

    isdr func

--- ai-research-code/d3net/music-source-separation/podcastmix_data/helpers.py
+++ ai-research-code/d3net/music-source-separation/podcastmix_data/helpers.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-
+import numpy as np
 
 def concatenate_eval_csv_files(folder_path):
     # Initialize an empty list to store DataFrames
@@ -22,3 +22,16 @@ def concatenate_eval_csv_files(folder_path):
 
     return concatenated_df
 
+
+def compute_ISDR(true_source, separated_signal):
+    # Compute energy of true source
+    true_source_energy = np.sum(np.square(np.abs(true_source)))
+
+    # Compute energy of spatial distortion
+    spatial_distortion = true_source - separated_signal
+    spatial_distortion_energy = np.sum(np.square(np.abs(spatial_distortion)))
+
+    # Compute ISDR
+    ISDR = true_source_energy / spatial_distortion_energy
+
+    return ISDR
